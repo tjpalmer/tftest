ff224f1d4bc71d8a112b94f9026b82fc9fa37be9
Tom <tjpalmer@tjpalmer.com>
2017-05-26 06:08:59 -0700

    TF tutorial and upcoming data plans

---
+++ hello.py
@@ -0,0 +1,112 @@
+def train_connected():
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+    import tensorflow as tf
+    sess = tf.Session()
+
+    x = tf.placeholder(tf.float32, shape=[None, 784])
+    y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+    W = tf.Variable(tf.zeros([784,10]))
+    b = tf.Variable(tf.zeros([10]))
+
+    sess.run(tf.global_variables_initializer())
+
+    with sess.as_default():
+        y = tf.matmul(x, W) + b
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+        train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+        for _ in range(1000):
+            batch = mnist.train.next_batch(100)
+            train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+
+        correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        print(accuracy.eval(
+            feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
+
+def train_conv():
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    import tensorflow as tf
+    sess = tf.Session()
+    # Inputs.
+    x = tf.placeholder(tf.float32, shape=[None, 784])
+    y_ = tf.placeholder(tf.float32, shape=[None, 10])
+    # Layers 1.
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    x_image = tf.reshape(x, [-1,28,28,1])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+    # Layers 2.
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+    # Dense.
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    # Dropout.
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    # Output.
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    # Train and test.
+    cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    sess.run(tf.global_variables_initializer())
+    print('test size: {}'.format(len(mnist.test.labels)))
+    with sess.as_default():
+        for i in range(20000):
+            batch = mnist.train.next_batch(50)
+            test_batch = mnist.test.next_batch(50)
+            if i % 100 == 0:
+                train_accuracy = accuracy.eval(feed_dict={
+                    x:batch[0], y_: batch[1], keep_prob: 1.0})
+                print("step %d, training accuracy %g" % (i, train_accuracy))
+                print("test accuracy %g" % accuracy.eval(feed_dict={
+                    x: test_batch[0],
+                    y_: test_batch[1],
+                    keep_prob: 1.0}))
+            train_step.run(
+                feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+
+def weight_variable(shape):
+    import tensorflow as tf
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    import tensorflow as tf
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    import tensorflow as tf
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    import tensorflow as tf
+    return tf.nn.max_pool(
+        x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+if __name__ == '__main__':
+    # train_connected()
+    train_conv()
