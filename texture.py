bd56aa6bad0113d417b86a4f1ebcc069a5f9272b
Tom <tjpalmer@tjpalmer.com>
2017-05-30 07:58:44 -0700

    Add broked model load

--- texture.py
+++ texture.py
@@ -1,4 +1,4 @@
-def build_autoencoder(shape, samples):
+def build_autoencoder(model_name, shape, samples):
     from hello import bias_variable, conv2d, max_pool_2x2, weight_variable
     from numpy import array
     import tensorflow as tf
@@ -73,6 +73,8 @@ def build_autoencoder(shape, samples):
     h_deconv1d = tf.nn.relu(conv2d(h_deconv1c, weight_variable([3, 3, 8, 8])))
     h_deconv1e = tf.nn.relu(conv2d(h_deconv1d, weight_variable([3, 3, 8, 8])))
     h_deconv1f = tf.nn.relu(conv2d(h_deconv1e, weight_variable([3, 3, 8, 1])))
+    # h_deconv1g = tf.nn.relu(conv2d(h_deconv1f, weight_variable([5, 5, 8, 8])))
+    # h_deconv1h = tf.nn.relu(conv2d(h_deconv1g, weight_variable([5, 5, 8, 1])))
     y_sample = tf.reshape(h_deconv1f, [-1, shape.prod()])
     # print(h_deconv1f.shape)
     # print(y_sample.shape)
@@ -85,6 +87,7 @@ def build_autoencoder(shape, samples):
     # W_fc2 = weight_variable([1024, 10])
     # b_fc2 = bias_variable([10])
     # y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    y_image = tf.reshape(y_sample, [-1] + list(shape))
     # Train and test.
     cross_entropy = tf.reduce_mean(tf.square(x - y_sample))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
@@ -94,50 +97,50 @@ def build_autoencoder(shape, samples):
     sess.run(tf.global_variables_initializer())
     # print('test size: {}'.format(len(mnist.test.labels)))
     batch_count = len(samples) // batch_size
+    saver = tf.train.Saver()
     with sess.as_default():
-        # result = h_conv2.eval({x: samples[:1]})
-        for i in range(25000):
-            start = (i % batch_count) * batch_size
-            batch = samples[start:start+batch_size]
-            # test_batch = mnist.test.next_batch(50)
-            if i % 100 == 0:
-                error = cross_entropy.eval({x: batch})
-                print('step {}, error {}'.format(i, error))
-            #     train_accuracy = accuracy.eval(feed_dict={
-            #         x:batch[0], y_: batch[1], keep_prob: 1.0})
-            #     print("step %d, training accuracy %g" % (i, train_accuracy))
-            #     print("test accuracy %g" % accuracy.eval(feed_dict={
-            #         x: test_batch[0],
-            #         y_: test_batch[1],
-            #         keep_prob: 1.0}))
-            train_step.run(feed_dict={x: batch})
-        # Save the model.
-        from datetime import datetime
-        from os import makedirs
-        from os.path import join
-        now = datetime.now()
-        time = now.strftime('%Y%m%d-%H%M%S')
-        name = 'texture-{}.cpkt'.format(time)
-        saver = tf.train.Saver()
-        dir_name = join('notes', 'models', time)
-        makedirs(dir_name)
-        out_name = saver.save(sess, join(dir_name, name))
-        print('Saved to {}'.format(out_name))
-        # Render sample output.
-        y_image = tf.reshape(y_sample, [-1] + list(shape))
-        images = []
-        for i in range(batch_count):
-            start = i * batch_size
-            batch = samples[start:start+batch_size]
-            batch_images = y_image.eval({x: batch})
-            images.extend(batch_images)
-            print('batch images size: {}'.format(batch_images.shape))
-        return images
+        if model_name:
+            saver.restore(sess, model_name)
+            print('Model loaded from {}'.format(model_name))
+        else:
+            # result = h_conv2.eval({x: samples[:1]})
+            for i in range(25000):
+                start = (i % batch_count) * batch_size
+                batch = samples[start:start+batch_size]
+                # test_batch = mnist.test.next_batch(50)
+                if i % 100 == 0:
+                    error = cross_entropy.eval({x: batch})
+                    print('step {}, error {}'.format(i, error))
+                #     train_accuracy = accuracy.eval(feed_dict={
+                #         x:batch[0], y_: batch[1], keep_prob: 1.0})
+                #     print(
+                #         "step %d, training accuracy %g" % (i, train_accuracy))
+                #     print("test accuracy %g" % accuracy.eval(feed_dict={
+                #         x: test_batch[0],
+                #         y_: test_batch[1],
+                #         keep_prob: 1.0}))
+                train_step.run(feed_dict={x: batch})
+            # Save the model.
+            from datetime import datetime
+            from os import makedirs
+            from os.path import join
+            now = datetime.now()
+            time = now.strftime('%Y%m%d-%H%M%S')
+            name = 'texture-{}.cpkt'.format(time)
+            dir_name = join('notes', 'models', time)
+            makedirs(dir_name)
+            out_name = saver.save(sess, join(dir_name, name))
+            print('Saved to {}'.format(out_name))
+    return batch_size, x, y_image
 
 
 def main():
+    from argparse import ArgumentParser
     from numpy import array, prod
     from scipy.misc import imread
+    parser = ArgumentParser()
+    parser.add_argument('--model')
+    args = parser.parse_args()
     image = imread('450px-Amethyst_gem_stone_texture_wwarby_flickr.jpg')
     image = image.mean(axis=-1)
     subs = split(image, 32)
@@ -147,11 +150,31 @@ def main():
     print(samples.shape)
     # show_image(image)
     # show_image_grid(subs)
-    images = build_autoencoder(subs.shape[2:], samples)
+    batch_size, x, y_image = build_autoencoder(
+        args.model, subs.shape[2:], samples)
+    images = render_images(batch_size, samples, x, y_image)
     images = array(images)
     images = images.reshape([10, 25] + list(images.shape[1:]))
     print('{}'.format(images.shape))
     show_image_grid(images)
+
+
+def render_images(batch_size, samples, x, y_image):
+    import tensorflow as tf
+    # Render sample output.
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    # print('test size: {}'.format(len(mnist.test.labels)))
+    batch_count = len(samples) // batch_size
+    with sess.as_default():
+        images = []
+        for i in range(batch_count):
+            start = i * batch_size
+            batch = samples[start:start+batch_size]
+            batch_images = y_image.eval({x: batch})
+            images.extend(batch_images)
+            print('batch images size: {}'.format(batch_images.shape))
+        return images
 
 
 def show_image(image):
